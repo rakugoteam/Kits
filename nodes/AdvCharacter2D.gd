9ca13e1802400e765af169d6f3342a6c0925bb74
Jeremi Biernacki <jeremi360@gmail.com>
2022-09-25 20:40:02 +0200

    clean up AdvCharacter2D and add character values for character talking

--- nodes/AdvCharacter2D.gd
+++ nodes/AdvCharacter2D.gd
@@ -1,49 +1,51 @@
 extends KinematicBody2D
 class_name AdventureCharacter2D, "res://addons/adventure-kit/icons/walk.svg"
 
+export var character_tag := "tag"
+export var character_name := "Name"
+export var character_text_color := Color.white
 export var speed := 100.0 setget _set_speed, _get_speed
 export var input_disabled := false setget _set_input_disabled, _get_input_disabled
 onready var _agent := $NavigationAgent2D as NavigationAgent2D
+onready var dialog_panel := $CharacterDialoguePanel
 
 var _velocity := Vector2.ZERO
 var walk_ended := true
 
+var is_talking := false setget , _get_talking
+var _talking := false
+
 signal walk(direction, speed)
 signal walk_end
-
+signal start_talking
+signal end_talking
 
 func _ready():
 	add_to_group("characters")
+	dialog_panel.character_tag = character_tag
+	dialog_panel.character_text_color = character_text_color
 
+	if !Rakugo.character_exists(character_tag):
+		Rakugo.define_character(character_tag, character_name)
+		Rakugo.set_character_variable(character_tag, "text_color", character_text_color)
+	
+	Rakugo.connect("say", self, "_on_say")
 
 func _set_input_disabled(disabled: bool):
 	set_process_input(!disabled)
 	_agent.avoidance_enabled = !disabled
 
-
 func _get_input_disabled() -> bool:
 	return !is_processing_input()
-
-
-# func _input(event):
-# 	if not event.is_action_pressed("left_click"):
-# 		return
-
-# 	var mouse_pos := get_global_mouse_position()
-# 	walk_to(mouse_pos)
-
 
 func walk_to(location: Vector2):
 	_agent.set_target_location(location)
 
-
 func _set_speed(value: float):
 	_agent.max_speed = value
 
-
 func _get_speed() -> float:
 	return _agent.max_speed
-
 
 func _process(delta):
 	if Input.is_action_just_pressed("left_click"):
@@ -52,17 +54,10 @@ func _process(delta):
 		walk_to(mouse_pos)
 		return
 
-	# var dir := Vector2.ZERO
-	# dir.y = Input.get_axis("ui_up", "ui_down")
-	# dir.x = Input.get_axis("ui_left", "ui_right")
-	# walk_to(global_position + dir * speed)
-
-
 func _physics_process(delta: float) -> void:
 	if _agent.is_navigation_finished():
 		if !walk_ended:
 			walk_ended = true
-			# emit_signal("walk", Vector2.ZERO, 0.0)
 			emit_signal("walk_end")
 		return
 
@@ -73,3 +68,16 @@ func _physics_process(delta: float) -> void:
 	_agent.set_velocity(_velocity)
 	move_and_collide(_velocity * delta)
 	emit_signal("walk", direction, _agent.max_speed)
+
+func _on_say(character:Dictionary, text:String):
+	if character_tag == character["tag"]:
+		if not _talking:
+			_talking = true
+			emit_signal("start_talking")
+
+	elif _talking:
+		_talking = false
+		emit_signal("end_talking")
+
+func _get_talking():
+	return _talking
