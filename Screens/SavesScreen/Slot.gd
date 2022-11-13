a343d975af2c002370867b4713a1d3f16de5abdc
Jeremi Biernacki <jeremi360@gmail.com>
2022-11-13 15:28:48 +0100

    a small code refactors

--- Screens/SavesScreen/Slot.gd
+++ Screens/SavesScreen/Slot.gd
@@ -16,13 +16,17 @@ var screenshot:ImageTexture = null
 func get_save_name(name:String, page_index:Vector2) -> String:
 	var page_index_str := "%s_%s" % [page_index.x, page_index.y]
 	var dir = Directory.new()
+
 	if dir.open(Rakugo.store_manager.save_folder_path) == OK:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
+
 		while file_name != "":
+
 			if !dir.current_is_dir():
 				if filename.begins_with(page_index_str):
 					return filename
+
 			file_name = dir.get_next()
 	
 	else:
@@ -33,6 +37,7 @@ func get_save_name(name:String, page_index:Vector2) -> String:
 func init(name:String, page_index:Vector2, hide_delete:bool = false, empty:bool = false):
 	if empty:
 		save_name = name
+		
 	else:
 		save_name = get_save_name(name, page_index)
 
@@ -54,13 +59,13 @@ func init(name:String, page_index:Vector2, hide_delete:bool = false, empty:bool 
 	
 	set_save_name(save_name)
 
+	$Button/VBoxContainer/Panel/DummySlot.visible = empty
+	$Button/VBoxContainer/DateLabel.visible = !empty
+
 	if empty:
 		set_data_time(0)
-		$Button/VBoxContainer/Panel/DummySlot.visible = true
-		$Button/VBoxContainer/DateLabel.visible = false
+
 	else:
-		$Button/VBoxContainer/Panel/DummySlot.visible = false
-		$Button/VBoxContainer/DateLabel.visible = true
 		set_data_time(file.get_modified_time(save_folder.plus_file(filename)))
 
 func load_screenshot_texture(path):
@@ -79,6 +84,7 @@ func load_screenshot_texture(path):
 
 func _on_save_select():
 	emit_signal("select_save", save_name, save_page_index)
+	print("select save", save_name, save_page_index)
 	
 func _on_save_delete():
 	if save_page_index:
@@ -93,8 +99,8 @@ func set_screenshot(texture):
 func set_data_time(data_time):
 	emit_signal("set_data_time", data_time)
 	
-func set_save_name(savename):
-	$Button/VBoxContainer/SaveNameLabel.text = savename
+func set_save_name(save_name:String):
+	$Button/VBoxContainer/SaveNameLabel.text = save_name
 	
 func hide_delete_button():
 	emit_signal("set_delete_button", false)
