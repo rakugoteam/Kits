b22e803685a8f42b9b0e920a66ce6b5f6fc6a94f
Jeremi Biernacki <jeremi360@gmail.com>
2022-12-01 09:10:51 +0100

    simpler and better code for opening menu tab

--- Screens/MenuBar/SubMenu.gd
+++ Screens/MenuBar/SubMenu.gd
@@ -1,22 +1,11 @@
 extends Control
 
-var menu_names:Dictionary = {
-	"history":"History",
-	"save":"Save",
-	"load":"Load",
-	"preferences":"Preferences",
-	"about":"About",
-	"help":"Help"
-}
 
 func _on_show_menu(menu, game_started):
 	prints("opening menu:", menu, "game_started:", game_started)
 	for nb in get_tree().get_nodes_in_group("nav_button"):
-		$CurrentSubMenu.visible = menu in menu_names
-		if menu in menu_names:
-			if nb.text == menu_names[menu]:
-				$CurrentSubMenu.text = menu_names[menu]
-				nb.pressed = true
+		$CurrentSubMenu.text = nb.text.capitalize()
+		nb.pressed = nb.toggle_mode
 
 		if "main_menu" in nb.get_groups():
 			nb.visible = !game_started
